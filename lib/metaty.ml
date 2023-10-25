128d51e08a70bca9a96bad667c177dbff176f1cc
Joseph Zullo <josephazullo@gmail.com>
2023-10-25 15:50:50 -0400

    Lay out basic syntactic unification

--- lib/metaty.ml
+++ lib/metaty.ml
@@ -32,6 +32,7 @@ and Free : sig  (* Boolean unifier for free boolean rings *)
   val unify : ubool -> ubool -> unit
   val bfresh : unit -> ubool
 end = Make(struct
+  (* Infinite Free Boolean Rings *)
 (* bool=false ==> 
      S.t Dict.t is the record with keys = string (tags) and values = S.t (types) 
    bool=true ==> 
@@ -52,8 +53,17 @@ and Unify : sig
 end = struct
 
   (* syntactic unification *)
-  let (=?) r = r |> unite ~sel:begin curry @@ function
-    | _ -> failwith "todo"
+  let rec (=?) r = r |> unite ~sel:begin curry @@ function
+    | S.MVar _, u | u, S.MVar _ -> u (* Add an occurs check *)
+    | S.MLit _ as u, v when u = v -> u
+    | MFun (i1, o1) as f, MFun (i2, o2) -> 
+      i1 =? i2;
+      o1 =? o2;
+      f
+    | TRec r1 as r, TRec r2 -> 
+      Free.unify r1 r2;
+      r
+    | _ -> failwith "Cannot unify distinct concrete types"
   end
 
 end
