702b2b74d00e5ae967959cb120177b0ecaf687af
Robochu <oleg.aprelikov@gmail.com>
2023-12-05 21:59:34 -0500

    Added function application to the interpreter

--- bin/main.ml
+++ bin/main.ml
@@ -32,7 +32,7 @@ let print_ctx ctx =
 
   
 
-let () = let lexbuf = Lexing.from_string "def main = 10 $" in
+let () = let lexbuf = Lexing.from_string "def fac n = if n = 0 then 1 else n * fac (n-1) end def main = fac (10) $" in
 let program = Parse.program_file Lex.token lexbuf in
 match interpret_defs program with
   | IntVal i -> print_endline (string_of_int i)
