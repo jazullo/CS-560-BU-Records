31d854a0f2c2161d5fc9279c758082af2efdd27e
Joseph Zullo <josephazullo@gmail.com>
2023-12-05 21:16:09 -0500

    Add command line logic

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,26 @@
-(* open! Batteries
+open! Batteries
 open! Brml
 open! Ast
+open Printf
+open Cli
 
-let () = let lexbuf = Lexing.from_string "def main = 19 $" in
-  let e = Parse.program_file Lex.token lexbuf in
-  let a, _ = List.hd e in
-  let _, _, b = a in
-  let c, _, _ = b in
-  assert (c = IntLit 19) *)
+let parse ch = 
+  let lexbuf = Lexing.from_channel ch in
+  Parse.program_file Lex.token lexbuf
+
+let check fname = 
+  let ast = parse (File.open_in fname) in
+  System.infer_defs Cyclic.empty ast
+
+let print_ctx ctx = 
+  Cyclic.to_list ctx |> List.iter @@ fun (name, ty) -> 
+    printf "%s : " name;
+    Types.Show.print_ty stdout ty;
+    printf "\n"
+
+(* let interpret = flags "interpret sources" "interpret" 'i' *)
+(* if O.get interpret then ... *)
+
+let () = match P.parse_argv op with
+  | [] | _ :: _ :: _ -> P.usage op ()
+  | [fname] -> print_ctx (check fname)
