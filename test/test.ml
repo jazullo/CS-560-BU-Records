34db3e399486e7724386f2212cfd6a4b97d6278f
Joseph Zullo <josephazullo@gmail.com>
2025-03-09 12:05:46 -0400

    Reenable working tests

--- test/test.ml
+++ test/test.ml
@@ -17,19 +17,19 @@ let gen str =
 let g = gen %> print_endline
 
 (* sanity checks *)
-(* let%expect_test _ = g "b0"; [%expect"b0"] *)
-(* let%expect_test _ = g "{x : bool}"; [%expect"{x : bool}"] *)
-(* let%expect_test _ = g "{x : bool} b0"; [%expect"{x : bool} b0"] *)
+let%expect_test _ = g "b0"; [%expect"b0"]
+let%expect_test _ = g "{x : bool}"; [%expect"{x : bool}"]
+let%expect_test _ = g "{x : bool} b0"; [%expect"{x : bool} b0"]
 
 (* lone polynomial tests *)
-(* let%expect_test _ = g "b0 b1"; [%expect"b0"] *)
-(* let%expect_test _ = g "b0 + b1"; [%expect"b1"] *)
+let%expect_test _ = g "b0 b1"; [%expect"b0"]
+let%expect_test _ = g "b0 + b1"; [%expect"b1"]
 
 let%expect_test _ = g "{x : bool} b0 b1"; [%expect"{x : bool} b0"]
-(* let%expect_test _ = g "{x : bool} (b0 + b1)"; [%expect"{x : bool} b1"] *)
+let%expect_test _ = g "{x : bool} (b0 + b1)"; [%expect"{x : bool} b1"]
 
-(* let%expect_test _ = g "b0 + {x : bool} b1"; [%expect"b1"] *)
-(* let%expect_test _ = g "{x : bool} b0 + !{x : bool} b1"; [%expect"b1"] *)
+let%expect_test _ = g "b0 + {x : bool} b1"; [%expect"b1"]
+let%expect_test _ = g "{x : bool} b0 + !{x : bool} b1"; [%expect"b1"]
 
 (* let%expect_test _ = g "{x : bool} b0 + !{y : bool} b1"; [%expect"!{y : bool} b1"] *)
 (* every cofinite constant, not just top, can be seen as some infinite product of finite constants *)
